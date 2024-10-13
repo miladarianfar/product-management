27681a518cb96df953956dc30a2c71ad19ec10b3
miladarianfar <miladarianfar8471@gmail.com>
2024-10-13 12:40:55 +0330

    feat: create domain model and repository

---
+++ src/domain/entities/product.entity.ts
@@ -0,0 +1,38 @@
+export class Product {
+  private id: number;
+  private name: string;
+  private price: number;
+  private stock: number;
+
+  constructor(id: number, name: string, price: number, stock: number) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.stock = stock;
+  }
+
+  public purchase(quantity: number): void {
+    if (quantity > this.stock) throw new Error('Insufficient stock');
+    this.stock -= quantity;
+  }
+
+  public getId(): number {
+    return this.id;
+  }
+
+  public getName(): string {
+    return this.name;
+  }
+
+  public getPrice(): number {
+    return this.price;
+  }
+
+  public getStock(): number {
+    return this.stock;
+  }
+
+  public setStock(stock: number): void {
+    this.stock = stock;
+  }
+}
