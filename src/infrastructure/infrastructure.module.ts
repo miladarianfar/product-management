ca64e289f808b4b0f00cecc2c06fc776b9d307c8
miladarianfar <miladarianfar8471@gmail.com>
2024-10-13 12:40:15 +0330

    feat: create infrastructure module

---
+++ src/infrastructure/infrastructure.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ProductEntity } from './database/product.entity';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: [ProductEntity],
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
+    TypeOrmModule.forFeature([ProductEntity]),
+  ],
+  providers: [],
+  exports: [TypeOrmModule],
+})
+export class InfrastructureModule {}
